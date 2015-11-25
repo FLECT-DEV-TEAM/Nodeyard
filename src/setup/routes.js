d130077967686d6abb63db28d65a4ff89bc84c1a
AKIRA-MIYAKE <akira.aktk.miyake@gmail.com>
2015-11-25 22:02:29 +0900

    Implement sample app
    テンプレートでのビューの描画とAPIの実装。
    bundle.jsとcontroller.js間での処理の共有。
    サーバとブラウザ間でのJSONの共有。

--- src/setup/routes.js
+++ src/setup/routes.js
@@ -1,12 +1,13 @@
 import home from '../controllers/render/home';
 
-import users from '../controllers/api/users';
+import greet from '../controllers/api/greet';
 
 
 export default function setup(app, passport, authorization) {
 
   app.use('/', home);
 
-  app.use('/api/users', users);
+  // API controllers is placed under "/api"
+  app.use('/api/greet', greet);
 
 }
--- test/sample.js
+++
@@ -1,9 +0,0 @@
-import chai from 'chai';
-
-describe('Array', function(){
-  describe('#indexOf()', function(){
-    it('should return -1 when not present', function(){
-      chai.expect('every').to.be.ok;
-    });
-  });
-});
